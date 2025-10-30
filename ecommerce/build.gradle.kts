15c0ff2cd9409706aedf178869f74f182579fd22
jay <234632793+jay05410@users.noreply.github.com>
2025-10-31 04:07:03 +0900

    chore: gradle 수정

--- ecommerce/build.gradle.kts
+++ ecommerce/build.gradle.kts
@@ -1,5 +1,8 @@
 plugins {
-    kotlin("jvm") version "2.1.20"
+    kotlin("jvm") version "1.9.21"
+    kotlin("plugin.spring") version "1.9.21"
+    id("org.springframework.boot") version "3.2.0"
+    id("io.spring.dependency-management") version "1.1.4"
 }
 
 group = "com.hanghae"
@@ -10,6 +13,10 @@ repositories {
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation(kotlin("test"))
 }
 
