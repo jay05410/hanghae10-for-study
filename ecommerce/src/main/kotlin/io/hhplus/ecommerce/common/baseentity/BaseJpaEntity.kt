184ba6d4a7cd9b5765ffc448b47432cde3ed07e7
jay <234632793+jay05410@users.noreply.github.com>
2025-11-07 09:23:54 +0900

    feat: base entity 설정

---
+++ ecommerce/src/main/kotlin/io/hhplus/ecommerce/common/baseentity/BaseJpaEntity.kt
@@ -0,0 +1,32 @@
+package io.hhplus.ecommerce.common.baseentity
+
+// import jakarta.persistence.MappedSuperclass
+// import jakarta.persistence.EntityListeners
+// import org.springframework.data.annotation.CreatedBy
+// import org.springframework.data.annotation.CreatedDate
+// import org.springframework.data.annotation.LastModifiedBy
+// import org.springframework.data.annotation.LastModifiedDate
+// import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.LocalDateTime
+
+// @MappedSuperclass
+// @EntityListeners(AuditingEntityListener::class)
+abstract class BaseJpaEntity(
+    // @CreatedBy
+    var createdBy: Long? = null,
+
+    // @CreatedDate
+    var createdAt: LocalDateTime = LocalDateTime.now(),
+
+    // @LastModifiedBy
+    var updatedBy: Long? = null,
+
+    // @LastModifiedDate
+    var updatedAt: LocalDateTime = LocalDateTime.now()
+) : SoftDeletableJpaEntity() {
+
+    fun updateAuditInfo(updatedBy: Long) {
+        this.updatedBy = updatedBy
+        this.updatedAt = LocalDateTime.now()
+    }
+}
