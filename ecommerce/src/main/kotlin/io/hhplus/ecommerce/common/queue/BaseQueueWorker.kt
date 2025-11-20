964e074998cd0814834743eb8cad0b1a0a903edf
jay <234632793+jay05410@users.noreply.github.com>
2025-11-21 01:04:11 +0900

    feat: 큐 처리용 공통 템플릿 생성

---
+++ ecommerce/src/main/kotlin/io/hhplus/ecommerce/common/queue/BaseQueueWorker.kt
@@ -0,0 +1,33 @@
+package io.hhplus.ecommerce.common.queue
+
+import org.slf4j.LoggerFactory
+
+/**
+ * Queue Worker 추상 클래스
+ *
+ * 역할:
+ * - dequeue → process → success/failure 플로우 자동화
+ */
+abstract class BaseQueueWorker<T, R>(
+    private val processor: QueueProcessor<T, R>
+) {
+    private val logger = LoggerFactory.getLogger(javaClass)
+
+    /**
+     * Queue 처리 메인 로직
+     */
+    protected fun processQueue() {
+        try {
+            val item = processor.dequeue() ?: return
+
+            try {
+                val result = processor.process(item)
+                processor.onSuccess(item, result)
+            } catch (e: Exception) {
+                processor.onFailure(item, e)
+            }
+        } catch (e: Exception) {
+            logger.error("Queue 처리 중 예상치 못한 오류", e)
+        }
+    }
+}
