aed3ff62ca6482734d1305e8725102a12b072ccf
jay <234632793+jay05410@users.noreply.github.com>
2025-11-26 01:00:32 +0900

    chore: 중복 의존성 삭제

--- ecommerce/build.gradle.kts
+++ ecommerce/build.gradle.kts
@@ -27,9 +27,6 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
-    // Spring Data JPA
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-
     // MySQL Database
     runtimeOnly(libs.mysql.connector)
 
